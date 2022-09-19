5151bcd203774c92d6b78e3b8517285468c61d8e
Florian Sinatra <22062145+FrankySnow@users.noreply.github.com>
2022-09-19 13:23:26 +0000

    install URQL devtools

--- app/src/boot/urql.ts
+++ app/src/boot/urql.ts
@@ -1,10 +1,12 @@
 import { boot } from 'quasar/wrappers'
-import urql from '@urql/vue'
+import urql, { defaultExchanges } from '@urql/vue'
 import authService from '../services/auth'
+import { devtoolsExchange } from '@urql/devtools'
 
 export default boot(async ({ app }) => {
   app.use(urql, {
     url: 'http://localhost/graphql',
+    exchanges: [devtoolsExchange, ...defaultExchanges],
     fetchOptions: () => {
       const token = authService.getToken()
       return {
